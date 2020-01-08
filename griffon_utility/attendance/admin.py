956e0a0b7e26d410719319fcbdb91413ed50b67c
Christopher Kozbial <ckozbial321@gmail.com>
2020-01-07 22:29:53 -0700

    Admin fancy-ed

--- griffon_utility/attendance/admin.py
+++ griffon_utility/attendance/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import Cadet, Day, Flight
 
+class DayAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('day info',       {'fields': ['date']}),
+		('cadets present', {'fields': ['cadetsPresent']}),
+	]
+
+class FlightAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('flight info', {'fields': ['name','nickname']}),
+		('cadets', {'fields': ['cadets']}),
+	]
+
 admin.site.register(Cadet)
-admin.site.register(Day)
-admin.site.register(Flight)
+admin.site.register(Day, DayAdmin)
+admin.site.register(Flight, FlightAdmin)
